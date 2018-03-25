581eb5e802d3f5bcb473d8bdd8919b2330c81afc
secnonsense <swinters@MacBook-Air-2.local>
2018-03-25 16:03:47 -0400

    Added numbers and upperlower

--- passdict.py
+++ passdict.py
@@ -7,22 +7,23 @@ from itertools import product
 fulldict={}
 
 parser = argparse.ArgumentParser()
-#parser.add_argument("-v", "--malware", help="Use known bad User Agent malware", action="store_true")
-#parser.add_argument("-o", "--openvas", help="Use known bad User Agent OpenVAS", action="store_true")
-#parser.add_argument("-z", "--meterpreter", help="Use known hacking tool User Agent Meterpreter", action="store_true")
-
-#parser.add_argument("-f", "--save", help="Save the body to a file", action="store_true")
+parser.add_argument("-n", "--numbers", help="Substitute 0-9 for any single digit", action="store_true")
+parser.add_argument("-u", "--upperlower", help="Subtitute lower/upper letters for each other", action="store_true")
 parser.add_argument("-l", "--leet", help="Use leetspeak substitutions for dictionary", action="store_true")
-parser.add_argument("-s", "--symbols", help="Specify a custom placeholder for symbol substitution", action="store", dest="symbol")
+parser.add_argument("-s", "--symbol", help="Specify a custom placeholder for symbol substitution", action="store", dest="symbol")
 parser.add_argument("WORDLIST", help="Enter a wordlist and its path")
 args = parser.parse_args()
 
-leetdict = {'a': '4@A', 'b': '8B', 'c': 'C', 'd': 'D', 'e': '3E', 'f': 'F', 'g': '6G', 'h': '4H', 
-'i': '1!I', 'j': 'J', 'l': '1L', 'm': 'M', 'n': 'N', 'o': '0O', 'p': 'P', 'q': 'Q', 'r': 'R','s': '$5S', 
-'t': '7T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': '2Z', 'A': 'a4@', 'B': 'b8','C': 'c', 
-'D': 'd', 'E': 'e3', 'F': 'f', 'G': 'g6', 'H': 'h4', 'I': 'i1!', 'J': 'j','L': 'l1', 'M': 'm', 'N': 'n',
-'O': 'o0', 'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's$5', 'T': 't7', 'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x',
-'Y': 'y','Z': 'z2'} 
+leetdict = {'a': '4@', 'b': '8', 'e': '3', 'g': '6', 'h': '4', 'i': '1!', 'l': '1', 'o': '0', 's': '$5', 
+'t': '7', 'z': '2', 'A': '4@', 'B': '8', 'E': '3', 'G': '6', 'H': '4', 'I': '1!', 'L': '1','O': '0', 'S': '$5',
+'T': '7', 'Z': '2'}
+
+upperlowerdict = {'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 
+'i': 'I', 'j': 'J', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R','s': 'S', 
+'t': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z', 'A': 'a', 'B': 'b','C': 'c', 
+'D': 'd', 'E': 'e', 'F': 'f', 'G': 'g', 'H': 'h', 'I': 'i', 'J': 'j','L': 'l', 'M': 'm', 'N': 'n',
+'O': 'o', 'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't', 'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x',
+'Y': 'y','Z': 'z'}
 
 symboldict = {args.symbol: '!@#$%^&*()_+=-`~;:"<>,./?\|[]\}\{\''} 
 
@@ -33,8 +34,15 @@ numberdict = {'1': '1234567890', '2': '1234567890',
 if args.leet:
     fulldict.update(leetdict)
 
-if args.symbols:
+if args.symbol:
     fulldict.update(symboldict)
+
+if args.upperlower:
+    fulldict.update(upperlowerdict)
+
+if args.numbers:
+    fulldict.update(numberdict)
+
 
 with open(args.WORDLIST) as infile:   
     data = infile.readlines()
