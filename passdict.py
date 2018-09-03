5efb51055494957ac05d32ef916a81ccbf673fbd
secnonsense <scott@securitynonsense.com>
2018-09-03 19:22:32 -0400

    updated comments

--- passdict.py
+++ passdict.py
@@ -23,6 +23,8 @@ args = parser.parse_args()
 if args.symbol:
     ksymbol=args.symbol
 
+#below are dictionaries used for substitutions
+
 leetdict = {'a': '4@', 'b': '8', 'e': '3', 'g': '6', 'h': '4', 'i': '1!', 'l': '1', 'o': '0', 's': '$5', 
 't': '7', 'z': '2', 'A': '4@', 'B': '8', 'E': '3', 'G': '6', 'H': '4', 'I': '1!', 'L': '1','O': '0', 'S': '$5',
 'T': '7', 'Z': '2'}
